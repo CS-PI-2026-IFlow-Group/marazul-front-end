741517d865d37a492f51fa7638604c64a02286f4
João Pinheiro <lucasdallas25@gmail.com>
2026-06-17 22:03:31 -0300

    feat: initialize language switcher and internationalization on page load

--- js/main.js
+++ js/main.js
@@ -4,14 +4,17 @@ import { initAboutCarousel } from "./index/carousel.js";
 import { loadFooter } from "./components/footer.js";
 import { initActiveNav } from "./modules/activeNav.js";
 import { initFAQ } from "./modules/faq.js";
+import { initI18n, initLangSwitcher } from "./modules/i18n.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  await loadHeader(); 
+  await loadHeader();
+  await loadFooter();
 
   initAboutCarousel();
   initFAQ();
-  loadFooter();
-  initActiveNav(); 
+  initActiveNav();
+  initLangSwitcher();
+  await initI18n();
 });
 
 window.addEventListener("load", () => {
